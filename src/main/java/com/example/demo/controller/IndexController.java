cba58afcf884ed5c2a616453ee6e771a904e5e58
pan <pan@cloudtogo.cn>
2018-08-02 02:37:13 +0800

    service01

---
+++ src/main/java/com/example/demo/controller/IndexController.java
@@ -0,0 +1,29 @@
+package com.example.demo.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * Created by cheney on 2018/8/2.
+ */
+@RestController
+public class IndexController {
+
+    private Logger logger = LoggerFactory.getLogger(getClass());
+
+    private SimpleDateFormat df = new SimpleDateFormat("yyyy-mm-dd HH:mm:ss");
+
+    @GetMapping("")
+    public String hello(String name) {
+        logger.info("name = {}", name);
+        if (null == name || name.trim().equals("")) {
+            return String.format("[%s]the name is null", df.format(new Date()));
+        }
+        return String.format("[%s]hello, %s!", df.format(new Date()), name);
+    }
+}
